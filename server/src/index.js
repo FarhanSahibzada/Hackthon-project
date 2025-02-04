c1c5bd90147e49a96195170614dcef13004741ad
Farhan Sahibzada <farhansahabzada3@gmail.com>
2025-02-04 23:20:12 +0500

    development bug fix

--- server/src/index.js
+++ server/src/index.js
@@ -2,14 +2,14 @@ import { DBconnect } from "./db/index.js";
 import { app } from "./app.js";
 import dotenv from 'dotenv'
 dotenv.config({
-    path: './.env'
+    path: '../.env'
 })
 
 DBconnect()
-    .then(() => {
+.then(() => {
         app.listen(process.env.PORT || 8000, () => {
-            console.log("SERVER IS RUNNing on port ", process.env.PORT || 8000)
+            console.log("SERVER IS RUNNing on port", process.env.PORT || 8000)
         })
-    }).catch((error) => {
+}).catch((error) => {
         console.log("mongodb connection is failed")
     }) 
