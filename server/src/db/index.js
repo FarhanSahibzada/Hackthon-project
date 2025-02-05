984208ae48a50d02513a852ae44aa83b93c04003
Farhan Sahibzada <farhansahabzada3@gmail.com>
2025-02-05 16:26:36 +0500

    development bug fix

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-dotenv.config({
-    path : "../.env"
-})
+dotenv.config()
 
 
 export const DBconnect = async () => {
     try {
-        const mongooseConnect = await mongoose.connect(`${process.env.MONOGODB_URL}/${process.env.DB_NAME}`)
+        const mongooseConnect = await mongoose.connect(process.env.MONOGODB_URL,{
+            dbName : process.env.DB_NAME,
+        })
         console.log("mongodb is connected ", mongooseConnect.connection.host);
     } catch (error) {
         console.log("error when conecting the db", error)
